43e4317cac965db3454114060b6ef1c0c86836c6
prasad ganta <prasadganta@prasads-MacBook-Air.local>
2022-06-03 22:10:40 +0530

    modified the file for http methods

--- src/main/java/com/techwithtony/sample/firstapp/controller/TestController.java
+++ src/main/java/com/techwithtony/sample/firstapp/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.techwithtony.sample.firstapp.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +13,27 @@ public class TestController {
 	@GetMapping("/hi")
 	public String sayHi() {
 		
-		return "Hi from first app!!!";
+		return "Hi from first app with Get Http method!!!";
 	}
+	
+	@PostMapping("/hi")
+	public String postMapping() {
+		
+		return "Hi from first app  with post Http method!!!!";
+	}
+	
+	@PutMapping("/hi")
+	public String putMapping() {
+		
+		return "Hi from first app   with put  Http method!!!";
+	}
+	
+	@DeleteMapping("/hi")
+	public String deleteMapping() {
+		
+		return "Hi from first app  with delete Http method!!!";
+	}
+	
+	
 
 }
