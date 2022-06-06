7747c4a83b038fab90af13cd5b905fc60f384a51
prasad ganta <prasadganta@prasads-MacBook-Air.local>
2022-06-06 21:49:11 +0530

    pathvariable and RequestParam

---
+++ src/main/java/com/techwithtony/sample/firstapp/controller/RequestParamAndPathVariables.java
@@ -0,0 +1,25 @@
+package com.techwithtony.sample.firstapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestParamAndPathVariables {
+	
+	@GetMapping("/mypath/myendpoint")
+	public String sample(@RequestParam(value="argument1") String argument1,@RequestParam(value="argument2") String argument2) {
+		
+		return "Hi from first app with Get Http method!!! along with RequestParams" +"argument1="+argument1 +", argument2="+argument2;
+	}
+	
+
+	
+	@GetMapping("/mypath/myendpoint/{pathvar1}")
+	public String sample2(@PathVariable(value="pathvar1") String pathvar1,@RequestParam(value="argument1") String argument1,@RequestParam(value="argument2") String argument2) {
+		
+		return "Hi from first app with Get Http method!!! along with RequestParams" +"argument1="+argument1 +", argument2="+argument2 +"And with one pathParams "+ pathvar1;
+	}
+	
+}
