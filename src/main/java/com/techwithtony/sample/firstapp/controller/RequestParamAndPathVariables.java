e93edb1dc64282d465be90101df7ad4a9816b70f
prasad ganta <prasadganta@prasads-MacBook-Air.local>
2022-06-09 18:51:14 +0530

    added docker file

--- src/main/java/com/techwithtony/sample/firstapp/controller/RequestParamAndPathVariables.java
+++ src/main/java/com/techwithtony/sample/firstapp/controller/RequestParamAndPathVariables.java
@@ -1,15 +1,19 @@
 package com.techwithtony.sample.firstapp.controller;
 
+import javax.validation.constraints.NotNull;
+
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 public class RequestParamAndPathVariables {
 	
 	@GetMapping("/mypath/myendpoint")
-	public String sample(@RequestParam(value="argument1") String argument1,@RequestParam(value="argument2") String argument2) {
+	public String sample(@RequestParam(value="argument1") @NotNull String argument1,@RequestParam(value="argument2") String argument2) {
 		
 		return "Hi from first app with Get Http method!!! along with RequestParams" +"argument1="+argument1 +", argument2="+argument2;
 	}
--- src/main/java/com/techwithtony/sample/firstapp/model/Person.java
+++ src/main/java/com/techwithtony/sample/firstapp/model/Person.java
@@ -1,32 +1,12 @@
 package com.techwithtony.sample.firstapp.model;
 
+import lombok.Data;
+
+@Data
 public class Person {
 	
 	private String firstName;
 	private String lastName;
-	private String address;
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	
-	
-	
-	
-	
+	private String address;	
 
 }
